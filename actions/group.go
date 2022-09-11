7cbabb6a4823b99f7f05feec7cc3e6e1f34eedf9
Ron <ronmachoka@gmail.com>
2022-09-11 22:21:54 +0300

    Change group identifier to namespace instead of group id

--- actions/group.go
+++ actions/group.go
@@ -8,7 +8,7 @@ import (
 	"thanos.cellulant.africa/cellulant-public/ci-cd-tools/packages/go/project-lookup/requests"
 )
 
-var GroupIds []int
+// var GroupIds []int
 var GitlabUrl = "https://thanos.cellulant.africa/"
 
 func GetGroupName(groupID int, authToken string) string {
@@ -21,21 +21,21 @@ func GetGroupName(groupID int, authToken string) string {
 	return fmt.Sprint(m["name"])
 }
 
-// Get group ids and store them in the global variable GroupIds
-func retrieveGroupIds(groups []int) {
-	for _, group := range groups {
-		GroupIds = append(GroupIds, group)
-	}
-}
+// // Get group ids and store them in the global variable GroupIds
+// func retrieveGroupIds(groups []int) {
+// 	for _, group := range groups {
+// 		GroupIds = append(GroupIds, group)
+// 	}
+// }
 
 // Check whether the group is among the ones being monitored or not
 // If it is, return true
 
-func CheckGroup(groups []int, groupID int) bool {
-	retrieveGroupIds(groups)
+func CheckGroup(groups []string, groupNamespace string) bool {
+	// retrieveGroupIds(groups)
 	exists := false
-	for _, x := range GroupIds {
-		if x == groupID {
+	for _, x := range groups {
+		if x == groupNamespace {
 			exists = true
 		}
 	}
